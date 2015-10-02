096e5465518af73fa2846af778b9c4814d542bc1
seksarn.tunsup <seksarn@izpal.com>
2015-10-02 16:33:52 +0700

    Last Update 2/10/2558

--- ParkingSpace.Web/Controllers/GateInController.cs
+++ ParkingSpace.Web/Controllers/GateInController.cs
@@ -10,14 +10,27 @@ namespace ParkingSpace.Web.Controllers {
 
   [RoutePrefix("gate-in")]
   public class GateInController : Controller {
+    private ParkingTicketService service;
+
+
+    public GateInController() {
+      service = new ParkingTicketService();
+    }
+    
     // GET: GateIn
     [Route]
     public ActionResult Index() {
       return View();
     }
 
+    [HttpPost]
+    [Route("CreateTicket")]
     public ActionResult CreateTicket(string plateno) {
-      throw new NotImplementedException();
+      var ticket = service.CreateParkingTicket(plateno);
+      printParkingTicket(ticket);
+      TempData["NewTicket"] = ticket;
+
+      return RedirectToAction("Index");
     }
 
     public ActionResult OpenBarrier() {
