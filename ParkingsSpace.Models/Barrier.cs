f3aa82ad3fe4ae647591e93755c4284ff6032c9d
seksarn.tunsup <seksarn@izpal.com>
2015-10-09 15:42:07 +0700

    test

---
+++ ParkingsSpace.Models/Barrier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingSpace.Models {
+  public class Barrier {
+    /// <summary>
+    /// ID of barrier
+    /// </summary>
+    public string ID { get; set; }
+    
+    /// <summary>
+    /// Status of barrier
+    /// </summary>
+    public BarrierStatus  Status { get; private set; }
+  }
+}
---
+++ ParkingsSpace.Models/Gate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ParkingSpace.Models {
+  public enum GateStatus {
+    Active = 1,
+    InActive = 2
+  }
+
+  public enum BarrierStatus {
+    Opened = 1,
+    Closed = 2,
+    OutOfService = 3
+  }
+
+  public  class Gate {
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public GateStatus Status { get; set; }
+
+  }
+}
